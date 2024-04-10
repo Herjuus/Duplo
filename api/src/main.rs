780c9aaab6d550f9c6097fead1d01c62835c6e12
herjuus <herjus420@gmail.com>
2024-04-10 10:20:41 +0200

    quick fix

--- api/src/main.rs
+++ api/src/main.rs
@@ -6,7 +6,8 @@ use serde::Serialize;
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get("Hello World"));
+        .route("/", get("Hello World"))
+        .route("/pods", get(get_deployments));
     
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
